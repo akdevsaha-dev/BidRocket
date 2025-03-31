2f10a7b1543fe10d45eabd39f69bcb594df0d929
Akdev Saha <akdevsaha2003@gmail.com>
2025-03-31 23:37:06 +0530

    prisma init

--- auth.ts
+++ auth.ts
@@ -1,11 +1,30 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    pages: {
-        signIn: "/signin"
-    },
+  adapter: PrismaAdapter(prisma),
+
   providers: [
-    Google,
+    Credentials({
+      credentials: {
+        firsname: { label: "Firstname" },
+        lastname: { label: "Lastname" },
+        email: { label: "email", type: "email" },
+        passwrod: { label: "Password", type: "password" },
+      },
+      authorize: async (credentails) => {
+        const { firstname, lastname, email, password } = credentails;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        return null;
+      },
+    }),
   ],
-})
+});
