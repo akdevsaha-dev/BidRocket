f4f88e8840985acff4e366dc5117d5ceb685deee
Akdev Saha <akdevsaha2003@gmail.com>
2025-06-08 01:14:34 +0530

    feat(auth): callback added

--- auth.ts
+++ auth.ts
@@ -75,4 +75,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       allowDangerousEmailAccountLinking: true,
     }),
   ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        // User is available during sign-in
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
 });
